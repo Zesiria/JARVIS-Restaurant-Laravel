f631d43fcebfd2e94cc5b63776accfd242fde216
Akanitt <akanit.d@ku.th>
2022-11-05 21:13:44 +0700

    add customer unit testing

--- tests/Unit/CustomerTest.php
+++ tests/Unit/CustomerTest.php
@@ -13,7 +13,6 @@ class CustomerTest extends TestCase
     {
         parent::setUp();
         Artisan::call('migrate');
-
     }
 
     protected function tearDown() : void
@@ -22,12 +21,18 @@ class CustomerTest extends TestCase
         parent::tearDown();
     }
 
-
     public function testSetNumberPeople()
     {
         $customer = new Customer();
         $customer->setNumberPeople(6);
         $this->assertEquals(6, $customer->getNumberPeople());
+    }
+
+    public function testSetNumberPeopleNegative()
+    {
+        $customer = new Customer();
+        $customer->setNumberPeople(-1);
+        $this->assertEquals(null, $customer->getNumberPeople());
     }
 
     public function testSetCode()
@@ -37,25 +42,12 @@ class CustomerTest extends TestCase
         $this->assertNotNull($customer->getCode());
     }
 
-    public function testTable()
+    public function testSetPrice()
     {
         $customer = new Customer();
-        $customer->belongsTo(Table::class);
-        $this->assertNotNull($customer->table());
+        $customer->setNumberPeople(1);
+        $customer->calculatePrice();
+        $price = (1 * 219 + 1 * 39) + ((1 * 219 + 1 * 39) * 0.1);
+        $this->assertEquals($price, $customer->getTotalPrice());
     }
-
-//    public function testOrder()
-//    {
-//        $customer = new Customer();
-//        $customer->hasMany(Order::class);
-//        $this->assertNotNull($customer->order());
-//    }
-//
-//    public function testFindCustomerById()
-//    {
-//        for ($i = 0; $i < 10; $i++) {
-//            $customer = new Customer();
-//        }
-//        $this->assertEquals(5, Customer::findCustomerById(5));
-//    }
 }
