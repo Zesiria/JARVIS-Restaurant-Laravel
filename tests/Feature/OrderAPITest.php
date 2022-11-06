2465687b3b77a60cc6c53b440a054c7a2c064770
Akanitt <akanit.d@ku.th>
2022-11-06 23:50:32 +0700

    add error function

--- tests/Feature/OrderAPITest.php
+++ tests/Feature/OrderAPITest.php
@@ -44,7 +44,74 @@ class OrderAPITest extends TestCase
         $response->assertStatus(200);
         $this->assertEquals(2, collect(json_decode($response->getContent()))->count());
     }
-    
+
+
+    // 500 Internal
+    public function test_get_order_from_api()
+    {
+        $food = new Food();
+        $food->setName('Shabu Pork');
+        $food->setType('meat');
+        $food->setQuantity(10);
+        $food->setImgPath('storage/images/meat/1.jpg');
+        $food->save();
+
+        $customer = new Customer();
+        $customer->number_people = 6;
+        $customer->code = 'FOOD';
+        $customer->save();
+
+        $table = new Table();
+        $table->setSize(6);
+        $table->save();
+
+        $order = new Order();
+        $order->customer_id = $customer->id;
+        $order->save();
+
+        $foodOrder = new FoodOrder();
+        $foodOrder->food_id = $food->id;
+        $foodOrder->order_id = $order->id;
+        $foodOrder->setQuantity(1);
+        $foodOrder->save();
+
+        $response = $this->get('/api/orders/1');
+        $response->assertStatus(200);
+        $response->assertJson(fn (AssertableJson $json) =>
+            $json->where('id', 1)
+                ->where('customer_id', 1)
+                ->where('status', 'PENDING')
+                ->etc());
+    }
+
+    // 500 Internal
+    public function test_post_order_to_api()
+    {
+        $customer = new Customer();
+        $customer->number_people = 6;
+        $customer->code = 'FOOD';
+        $customer->save();
+
+        $food = new Food();
+        $food->setName('Shabu Pork');
+        $food->setType('meat');
+        $food->setQuantity(10);
+        $food->setImgPath('storage/images/meat/1.jpg');
+        $food->save();
+
+        $order = new Order();
+        $order->customer_id = $customer->id;
+        $order->save();
+
+//        $food_order = new FoodOrder();
+//        $food_order->food_id = $food->id;
+//        $food_order->order_id = $order->id;
+//        $food_order->setQuantity(1);
+//        $food_order->save();
+
+        $response = $this->postJson('api/orders', ["customer_id" => 1, "foodOrders" => ['food_id' => 1, 'orderQuantity' => 1]]);
+        $response->assertStatus(201);
+    }
 
     public function test_put_order_to_api()
     {
